5aba7ce37b2bc2fab8f31354f7f73212e76478a0
Eugene Korbut <me@mvl.ru>
2020-02-10 00:32:14 +0300

    custom layer modificator

--- keyboards/redox/keymaps/vlmonk/keymap.c
+++ keyboards/redox/keymaps/vlmonk/keymap.c
@@ -5,10 +5,26 @@
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+enum {
+  SINGLE_TAP = 1,
+  SINGLE_HOLD = 2,
+  DOUBLE_TAP = 3
+};
+
+int cur_dance (qk_tap_dance_state_t *state);
+void left_mod_finished(qk_tap_dance_state_t *state, void *user_data);
+void left_mod_reset(qk_tap_dance_state_t *state, void *user_data);
+
 //Tap Dance Declarations
 enum {
   TD_LEFT_BR = 0,
-  TD_RGHT_BR
+  TD_RGHT_BR,
+  TD_LEFT_MOD
 };
 
 //Tap Dance Definitions
@@ -16,7 +32,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   // single tap for '[', double tap for '{'
   [TD_LEFT_BR]  = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_LCBR),
   // single tap for ']', double tap for '}'
-  [TD_RGHT_BR]  = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR)
+  [TD_RGHT_BR]  = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_RCBR),
+  [TD_LEFT_MOD] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, left_mod_finished, left_mod_reset, 198)
 // Other declarations would go here, separated by commas, if you have them
 };
 
@@ -40,6 +57,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define MY_RBR  TD(TD_RGHT_BR)
 
 #define MY_SPC   MT(MOD_RGUI, KC_SPC)
+#define MY_LMD   TD(TD_LEFT_MOD)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
@@ -52,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSPO ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,LT2_PU  ,KC_PGDN ,        KC_HOME ,LT2_END ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSPC ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTL ,LNV_PLS ,KC_PMNS ,AL_PAST ,     KC_NO   ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,MY_SPC  ,    KC_NO   ,     AL_LEFT ,KC_DOWN ,KC_UP   ,CTL_R
+     KC_LCTL ,LNV_PLS ,KC_PMNS ,AL_PAST ,     KC_NO   ,    KC_BSPC ,KC_DEL  ,        KC_ENT  ,MY_SPC  ,    MY_LMD  ,     AL_LEFT ,KC_DOWN ,KC_UP   ,CTL_R
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -83,3 +101,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   )
 };
+
+
+
+int cur_dance(qk_tap_dance_state_t *state) {
+  if (state->count == 1) {
+    if (!state->pressed) {
+      return SINGLE_TAP;
+    } else {
+      return SINGLE_HOLD;
+    }
+  } else if (state->count == 2) {
+    return DOUBLE_TAP;
+  } else {
+    return 8;
+  }
+}
+
+static tap left_mod_tap = {
+  .is_press_action = true,
+  .state = 0
+};
+
+void left_mod_finished(qk_tap_dance_state_t *state, void *user_data) {
+  left_mod_tap.state = cur_dance(state);
+  switch (left_mod_tap.state) {
+    case SINGLE_TAP:
+      set_oneshot_layer(_SYMB, ONESHOT_START);
+      clear_oneshot_layer_state(ONESHOT_PRESSED);
+      break;
+    case SINGLE_HOLD:
+      layer_on(_SYMB);
+      break;
+    case DOUBLE_TAP:
+      layer_on(_SYMB);
+      break;
+  }
+}
+
+void left_mod_reset(qk_tap_dance_state_t *state, void *user_data) {
+  switch (left_mod_tap.state) {
+    case SINGLE_HOLD:
+      layer_off(_SYMB);
+      break;
+  }
+  left_mod_tap.state = 0;
+}
