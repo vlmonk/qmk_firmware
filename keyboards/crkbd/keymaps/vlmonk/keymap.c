923db58dfc84a02e9cd37f0630d064b5a7b6108a
Eugene Korbut <me@mvl.ru>
2020-04-18 18:21:30 +0300

    Update my layout

--- keyboards/crkbd/keymaps/vlmonk/keymap.c
+++ keyboards/crkbd/keymaps/vlmonk/keymap.c
@@ -39,12 +39,15 @@ enum macro_keycodes {
 #define KC_J_SYM LT(_SYMB_L, KC_J)
 #define KC_F_SYM LT(_SYMB_R, KC_F)
 
+// ctrl / esc button
+#define KC_CESC MT(MOD_LCTL, KC_ESC)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_EQL  ,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     KC_LCTL ,KC_A    ,KC_S    ,KC_D    ,KC_F_SYM,KC_G    ,                     KC_H    ,KC_J_SYM,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,\
+     KC_CESC ,KC_A    ,KC_S    ,KC_D    ,KC_F_SYM,KC_G    ,                     KC_H    ,KC_J_SYM,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
