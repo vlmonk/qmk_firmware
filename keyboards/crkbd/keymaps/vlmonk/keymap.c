30d0f09f00203e0bf7e3776202642e425e11235f
Eugene Korbut <me@mvl.ru>
2020-07-08 23:28:42 +0300

    massive simplifiaction

--- keyboards/crkbd/keymaps/vlmonk/keymap.c
+++ keyboards/crkbd/keymaps/vlmonk/keymap.c
@@ -10,8 +10,7 @@ extern uint8_t is_master;
 
 enum layers {
   _QWERTY,
-  _SYMB_L,
-  _SYMB_R,
+  _SYMB,
   _NAV,
   _NUM,
 };
@@ -45,26 +44,9 @@ static bool alt_pressed = false;
 void double_alt_done(qk_tap_dance_state_t *state, void *user_data);
 void double_alt_reset(qk_tap_dance_state_t *state, void *user_data);
 
-/* static int w_tap_state = 0; */
-void w_tap(qk_tap_dance_state_t *state, void *user_data);
-void w_finished(qk_tap_dance_state_t *state, void *user_data);
-void w_reset(qk_tap_dance_state_t *state, void *user_data);
-
-void j_tap(qk_tap_dance_state_t *state, void *user_data);
-void j_finished(qk_tap_dance_state_t *state, void *user_data);
-void j_reset(qk_tap_dance_state_t *state, void *user_data);
-
-void f_tap(qk_tap_dance_state_t *state, void *user_data);
-void f_finished(qk_tap_dance_state_t *state, void *user_data);
-void f_reset(qk_tap_dance_state_t *state, void *user_data);
-
-
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_DOUBLE_ALT] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(NULL, double_alt_done, double_alt_reset, 200),
-  [TD_WNUM] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(w_tap, w_finished, w_reset, 200),
-  [TD_JKEY] = ACTION_TAP_DANCE_FN_ADVANCED(j_tap, j_finished, j_reset),
-  [TD_FKEY] = ACTION_TAP_DANCE_FN_ADVANCED_TIME(f_tap, f_finished, f_reset, 200),
 };
 
 
@@ -73,8 +55,8 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define KC_DNUM LT(_NUM, KC_D)
 #define KC_WNUM TD(TD_WNUM)
 #define KC_DALT TD(TD_DOUBLE_ALT)
-#define KC_FKEY TD(TD_FKEY)
-#define KC_JKEY TD(TD_JKEY)
+#define KC_FKEY LT(_SYMB, KC_F)
+#define KC_JKEY LT(_SYMB, KC_J)
 
 // ctrl / esc button
 #define KC_CESC MT(MOD_LCTL, KC_ESC)
@@ -101,25 +83,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [_SYMB_L] = LAYOUT( \
+  [_SYMB] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     _______ ,KC_GRV  ,KC_AT   ,KC_PIPE ,KC_LCBR ,KC_RCBR ,                     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,\
+     _______ ,KC_GRV  ,KC_AT   ,KC_PIPE ,KC_LCBR ,KC_RCBR ,                     KC_BSLS ,KC_ASTR ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     _______ ,KC_DQUO ,KC_EXLM ,KC_DLR  ,KC_LPRN ,KC_RPRN ,                     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,\
+     _______ ,KC_DQUO ,KC_EXLM ,KC_DLR  ,KC_LPRN ,KC_RPRN ,                     KC_MINS ,KC_AMPR ,KC_HASH ,XXXXXXX ,KC_COLN ,_______ ,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     _______ ,KC_QUOT ,KC_TILD ,KC_PERC ,KC_LBRC ,KC_RBRC ,                     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,\
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                         _______ ,_______ ,_______ ,   _______ ,_______ ,_______ \
-                                      //`--------------------------'  `--------------------------'
-  ),
-
-  [_SYMB_R] = LAYOUT( \
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                     KC_BSLS ,KC_ASTR ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                     KC_MINS ,KC_AMPR ,KC_HASH ,XXXXXXX ,KC_COLN ,_______ ,\
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-     _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                     KC_UNDS ,KC_CIRC ,KC_LABK ,KC_RABK  ,XXXXXXX ,_______ ,\
+     _______ ,KC_QUOT ,KC_TILD ,KC_PERC ,KC_LBRC ,KC_RBRC ,                     KC_UNDS ,KC_CIRC ,KC_LABK ,KC_RABK  ,XXXXXXX ,_______ ,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                          _______ ,_______ ,_______ ,   _______ ,_______ ,_______ \
                                       //`--------------------------'  `--------------------------'
@@ -245,47 +215,47 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     dprintf("process_record_user: keycode %d released\n", keycode);
   }
 
-  uint8_t layer = biton32(layer_state);
+  /* uint8_t layer = biton32(layer_state); */
 
-  switch (remember.step) {
-    case COM_NONE:
-      if (record->event.pressed && (keycode == KC_FKEY || keycode == KC_JKEY)) {
-        dprintf("OK, step one detected, remember %d at (%d / %d)\n", keycode, record->event.key.row, record->event.key.col);
-        remember.step = COM_ONE;
-        remember.first.col = record->event.key.col;
-        remember.first.row = record->event.key.row;
-      }
-      break;
-    case COM_ONE:
-      if (record->event.pressed) {
-        dprintf("OK, step two detected, remember %d\n", keycode);
-        remember.last.col = record->event.key.col;
-        remember.last.row = record->event.key.row;
-        remember.step = COM_TWO;
-        return false;
-      } else {
-        remember.step = COM_NONE;
-      }
+  /* switch (remember.step) { */
+  /*   case COM_NONE: */
+  /*     if (record->event.pressed && (keycode == KC_FKEY || keycode == KC_JKEY)) { */
+  /*       dprintf("OK, step one detected, remember %d at (%d / %d)\n", keycode, record->event.key.row, record->event.key.col); */
+  /*       remember.step = COM_ONE; */
+  /*       remember.first.col = record->event.key.col; */
+  /*       remember.first.row = record->event.key.row; */
+  /*     } */
+  /*     break; */
+  /*   case COM_ONE: */
+  /*     if (record->event.pressed) { */
+  /*       dprintf("OK, step two detected, remember %d\n", keycode); */
+  /*       remember.last.col = record->event.key.col; */
+  /*       remember.last.row = record->event.key.row; */
+  /*       remember.step = COM_TWO; */
+  /*       return false; */
+  /*     } else { */
+  /*       remember.step = COM_NONE; */
+  /*     } */
 
-      break;
-    case COM_TWO:
-      if (record->event.pressed) {
-        dprintf("OK, step three, press %d\n", keycode);
-        register_code16(keymap_key_to_keycode(layer, remember.last));
-      } else {
-        if (remember.first.col == record->event.key.col && remember.first.row == record->event.key.row) {
-          dprintf("Variant 1\n");
-          tap_code16(qwerty_key(&remember.first));
-          tap_code16(qwerty_key(&remember.last));
-        } else {
-          dprintf("Variant 3\n");
-          register_code16(keymap_key_to_keycode(layer, remember.last));
-        }
-      }
+  /*     break; */
+  /*   case COM_TWO: */
+  /*     if (record->event.pressed) { */
+  /*       dprintf("OK, step three, press %d\n", keycode); */
+  /*       register_code16(keymap_key_to_keycode(layer, remember.last)); */
+  /*     } else { */
+  /*       if (remember.first.col == record->event.key.col && remember.first.row == record->event.key.row) { */
+  /*         dprintf("Variant 1\n"); */
+  /*         tap_code16(qwerty_key(&remember.first)); */
+  /*         tap_code16(qwerty_key(&remember.last)); */
+  /*       } else { */
+  /*         dprintf("Variant 3\n"); */
+  /*         register_code16(keymap_key_to_keycode(layer, remember.last)); */
+  /*       } */
+  /*     } */
 
-      remember.step = COM_NONE;
-      break;
-  }
+  /*     remember.step = COM_NONE; */
+  /*     break; */
+  /* } */
 
   return true;
 }
@@ -307,135 +277,4 @@ void double_alt_reset(qk_tap_dance_state_t *state, void *user_data) {
   if (alt_pressed) {
     unregister_mods(MOD_BIT(KC_RALT));
   }
-}
-
-void w_tap(qk_tap_dance_state_t *state, void *user_data) {
-  state->finished = true;
-  dprint("w_tap\n");
-}
-
-void w_reset(qk_tap_dance_state_t *state, void *user_data) {
-  dprint("w_reset\n");
-}
-
-void w_finished(qk_tap_dance_state_t *state, void *user_data) {
-  dprint("w_finished\n");
-}
-
-int cur_dance(qk_tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (state->pressed) {
-      return SINGLE_HOLD;
-    } else {
-      return SINGLE_TAP;
-    }
-  } else if (state->count == 2) {
-    if (state->pressed) {
-      return DOUBLE_HOLD;
-    } else {
-      return DOUBLE_TAP;
-    }
-  } else  {
-    return UNKNOWN_TAPHOLD;
-  }
-}
-
-enum {
-  J_NONE,
-  J_LAYER,
-  J_HOLD
-};
-
-int j_status = J_NONE;
-
-void j_tap(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("j_tap, count: %d\n", state->count);
-
-  if (state->count == 1) {
-    layer_on(_SYMB_L);
-    j_status = J_LAYER;
-  } else if (state->count == 2) {
-    layer_off(_SYMB_L);
-    register_code(KC_J);
-    unregister_code(KC_J);
-    register_code(KC_J);
-    j_status = J_HOLD;
-  } else {
-    unregister_code(KC_J);
-    register_code(KC_J);
-  }
-}
-
-void j_finished(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("j_finished, count: %d, pressed: %d\n", state->count, state->pressed);
-
-  if (j_status == J_LAYER && !state->pressed) {
-    register_code(KC_J);
-    unregister_code(KC_J);
-  }
-}
-
-void j_reset(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("j_reset, count: %d, pressed: %d\n", state->count, state->pressed);
-
-  switch (j_status) {
-    case J_LAYER:
-      layer_off(_SYMB_L);
-      break;
-    case J_HOLD:
-      unregister_code(KC_J);
-      break;
-  }
-
-  j_status = J_NONE;
-}
-
-enum {
-  F_NONE,
-  F_LAYER,
-  F_HOLD
-};
-
-int f_status = F_NONE;
-
-void f_tap(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("f_tap, count: %d\n", state->count);
-
-  if (state->count == 1) {
-    layer_on(_SYMB_R);
-    f_status = F_LAYER;
-  } else if (state->count == 2) {
-    layer_off(_SYMB_R);
-    register_code(KC_F);
-    unregister_code(KC_F);
-    register_code(KC_F);
-    f_status = F_HOLD;
-  } else {
-    unregister_code(KC_F);
-    register_code(KC_F);
-  }
-}
-
-void f_finished(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("f_finished, count: %d, pressed: %d\n", state->count, state->pressed);
-
-  if (f_status == F_LAYER && !state->pressed) {
-    register_code(KC_F);
-    unregister_code(KC_F);
-  }
-}
-
-void f_reset(qk_tap_dance_state_t *state, void *user_data) {
-  dprintf("f_reset, count: %d, pressed: %d\n", state->count, state->pressed);
-
-  switch (f_status) {
-    case F_LAYER:
-      layer_off(_SYMB_R);
-      break;
-    case F_HOLD:
-      unregister_code(KC_F);
-      break;
-  }
-
-  f_status = F_NONE;
 }
