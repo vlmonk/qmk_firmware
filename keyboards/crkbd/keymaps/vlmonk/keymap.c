3b764c0a7844a02e4cb312d70ca826051ba65c57
Eugene Korbut <me@mvl.ru>
2020-07-08 22:34:21 +0300

    Cleanup unused keycodes

--- keyboards/crkbd/keymaps/vlmonk/keymap.c
+++ keyboards/crkbd/keymaps/vlmonk/keymap.c
@@ -8,11 +8,6 @@ extern rgblight_config_t rgblight_config;
 
 extern uint8_t is_master;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-//
 enum layers {
   _QWERTY,
   _SYMB_L,
@@ -22,11 +17,7 @@ enum layers {
 };
 
 enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  LOWER,
-  RAISE,
-  BACKLIT,
-  RGBRST
+  MK_F = SAFE_RANGE
 };
 
 enum tap_dance {
